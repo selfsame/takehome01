8e13ea9255797e7485755e2bdc1b73a6a48db3fd
selfsame <n/a>
2024-03-11 01:17:08 -0500

    viable pagination, checkout

--- src/gravie_test/core.cljs
+++ src/gravie_test/core.cljs
@@ -5,12 +5,21 @@
     [ajax.core :refer [GET POST]]
     [cljs.pprint :as pprint]))
 
+(declare search!)
+
 (defonce state (r/atom {
   :view :search
   :cart #{}
   :working false
+  :search-term nil
   :page 0
-  :search-results nil}))
+  :search-results nil
+  :games/by-id {}}))
+
+(add-watch state :page-change
+  (fn [k r o n]
+    (when (not= (:page o) (:page n))
+      (search! (:search-term @state)))))
 
 (defn price [game]
   (let [digits (str (js/Math.abs (hash (:name game))))]
@@ -19,19 +28,25 @@
 (defn search! [term]
   (swap! state assoc :working true)
   (GET (str "api/search?filter=name:" term 
+    "&offset=" (* (:page @state) 10)
     "&field_list=id,name,image,deck,original_release_date&limit=10&sort=original_release_date:desc")
     :response-format :json
     :keywords? true
     :handler (fn [res] 
                ;TODO store in games/by-id
+               (swap! state update :games/by-id merge 
+                 (into {} (map (juxt :id identity) (:results res))))
                (swap! state assoc :search-results res)
                (swap! state assoc :working false))))
 
 (defn header-ui []
-  [:div.header
-    [:div.logotype "GameRenter"]
-    [:div {:on-click (fn [e] (swap! state assoc :view :search))} "Search Games"]
-    [:div.cart {:on-click (fn [e] (swap! state assoc :view :checkout))} "Checkout"]])
+  (let [cnt (count (:cart @state))]
+    [:div.header
+      [:div.section
+        [:div.logotype {:on-click (fn [e] (swap! state assoc :view :search))} 
+          "GameRenter"]
+        [:div.cart {:on-click (fn [e] (swap! state assoc :view :checkout))} 
+          "Checkout" (when (pos? cnt) (str " (" cnt ")"))]]]))
 
 (defn game-ui [o]
   (let [rented? (contains? (:cart @state) (:id o))]
@@ -52,36 +67,62 @@
             (str "rent for $" (price o)))]]]))
 
 (defn pagination []
-  [:div.pagination
-    [:div "<"]
-    [:div "page " (inc (:page @state)) " of " (:number_of_total_results (:search-results @state))]
-    [:div ">"]])
+  (let [page-cnt (js/Math.ceil (/ (:number_of_total_results (:search-results @state)) 10))]
+    [:div.pagination
+      (if (= (:page @state) 0)
+        [:div.arrow.disabled "<"]
+        [:div.arrow 
+          {:on-click (fn [e] (swap! state update :page dec))}
+          "<"])
+      [:div "page " (inc (:page @state)) " of " page-cnt]
+      (if (>= (inc (:page @state)) page-cnt)
+        [:div.arrow.disabled ">"]
+        [:div.arrow 
+          {:on-click (fn [e] (swap! state update :page inc))}
+          ">"])]))
 
 (defn search-ui []
-  [:div.section
-    [:div.search
-      [:input 
-       {:value (:search-term @state)
-        :on-change (fn [e] (swap! state assoc :search-term (-> e .-target .-value )))
-        :on-key-up (fn [e] (when (or (= (.-key e) "Enter")
-                                    (= (.-keyCode e) 13))
-                            (search! (:search-term @state))))}]
-      [:button 
-        {:on-click (fn [e] (search! (:search-term @state)))} 
-        "SEARCH GAMES"]]
-    (if (:working @state)
-      [:h1 "working.."]
-      [:div
-        [pagination]
-        (into [:div.results]
-          (map 
-            game-ui
-            (-> @state :search-results :results)))
-        [pagination]])])
+  (let [search-fn (fn [] 
+                    (swap! state assoc :page 0)
+                    (search! (:search-term @state)))]
+    [:div.section
+      [:div.search
+        [:input 
+        {:value (:search-term @state)
+          :on-change (fn [e] (swap! state assoc :search-term (-> e .-target .-value )))
+          :on-key-up (fn [e] (when (or (= (.-key e) "Enter")
+                                      (= (.-keyCode e) 13))
+                              (search-fn)))}]
+        [:div.button {:on-click (fn [e] (search-fn))} "SEARCH"]]
+      (if (:working @state)
+        [:h1 "working.."]
+        [:div
+          [pagination]
+          (let [games (-> @state :search-results :results)]
+            (if (empty? games)
+              [:h1 "No Results"]
+              (into [:div.results]
+                (map game-ui games))))
+          [pagination]])]))
+
+(defn cart-item [o]
+  [:div.cart-item.flexrow
+    [:p [:button {:on-click (fn [e] (swap! state update :cart disj (:id o)))} "x"]
+        (:name o)]
+    [:p "$" (price o)]])
 
 (defn checkout-ui []
-  [:div.section
-    [:h3 "Checkout"]])
+  (let [games (map #(get-in @state [:games/by-id %]) (:cart @state))]
+    [:div.section.checkout
+      [:h3 "Checkout"]
+      (into [:div.items]
+        (map cart-item games))
+      [:div.flexrow
+        [:p "TOTAL"]
+        [:p (str "$" (reduce + (map (comp js/parseFloat price) games)))]]
+      [:div.button 
+        {:on-click (fn [e] (swap! state merge {:cart #{} :view :search}))}
+        "CHECKOUT"]]))
 
 (defn root-ui []
   [:div
@@ -89,9 +130,7 @@
     (case (:view @state)
       :search [search-ui]
       :checkout [checkout-ui]
-      [:p "unknown view: " (:view @state)])
-    ;[:pre (with-out-str (pprint/pprint @state))]
-    ])
+      [:p "unknown view: " (:view @state)])])
 
 (rd/render [root-ui] (js/document.querySelector "#app"))
 
